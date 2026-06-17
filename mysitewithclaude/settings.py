65cfb00187e059c91e66239bcfbddf8f441d7c50
jaliljj <jalilshams74@gmail.com>
2026-06-17 20:20:55 +0330

    Use env variable for DEBUG

--- mysitewithclaude/settings.py
+++ mysitewithclaude/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yoiv5h9&kjp9ha^n#q$wkpv*w)k4-fz$l-rx_2#3m#m8^%ix5e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['jalilshams.pythonanywhere.com', '127.0.0.1', 'localhost']
 
